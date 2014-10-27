ac399b0b23a050dd923c88ebc14e4a10e40dbc68
Scott Killen <scott@scottkillen.com>
2014-10-27 01:49:41 -0400

    Correct Kiparis tree gen.

--- src/main/java/com/scottkillen/mod/dendrology/world/gen/feature/KiparisTree.java
+++ src/main/java/com/scottkillen/mod/dendrology/world/gen/feature/KiparisTree.java
@@ -6,6 +6,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 import java.util.Random;
 
+@SuppressWarnings({ "OverlyComplexBooleanExpression", "MethodWithMoreThanThreeNegations", "MethodWithMultipleLoops" })
 public class KiparisTree extends AbstractTree
 {
     public KiparisTree(boolean isFromSapling)
@@ -42,82 +43,105 @@ public class KiparisTree extends AbstractTree
         return super.isReplaceable(world, x, y, z) || world.getBlock(x, y, z).getMaterial().equals(Material.water);
     }
 
-    @SuppressWarnings({
-            "MethodWithMultipleLoops",
-            "OverlyComplexMethod",
-            "OverlyComplexBooleanExpression",
-            "MethodWithMoreThanThreeNegations",
-            "OverlyLongMethod",
-            "OverlyNestedMethod",
-            "ConstantConditions"
-    })
+    @SuppressWarnings("OverlyComplexMethod")
     @Override
-    public boolean generate(World world, Random rand, int x, int y, int z)
+    public boolean generate(World world, Random random, int i, int j, int k)
     {
-        final int size = 1 + (rand.nextInt(7) < 2 ? 1 : 0) + (rand.nextInt(7) < 2 ? 1 : 0) + rand.nextInt(2);
+        final int size = 1 + (random.nextInt(7) < 2 ? 1 : 0) + (random.nextInt(7) < 2 ? 1 : 0) + (random.nextInt(2) == 0 ? 1 : 0);
         final int height = 4 * size + 1;
 
-        if (isPoorGrowthConditions(world, x, y, z, height, ModBlocks.sapling0)) return false;
+        if (isPoorGrowthConditions(world, i, j, k, height, ModBlocks.sapling0)) return false;
 
-        final Block block = world.getBlock(x, y - 1, z);
-        block.onPlantGrow(world, x, y - 1, z, x, y, z);
+        final Block block = world.getBlock(i, j - 1, k);
+        block.onPlantGrow(world, i, j - 1, k, i, j, k);
 
-        for (int jn = 0; jn <= height; jn++)
+        for (int dY = 0; dY <= height; dY++)
         {
-            if (jn != height) placeLog(world, x, y + jn, z);
+            if (dY != height) placeLog(world, i, j + dY, k);
 
-            if (size == 1 && jn > 0) for (int dX = -1; dX <= 1; dX++)
-                for (int dZ = -1; dZ <= 1; dZ++)
-                    if (Math.abs(dX) != 1 || Math.abs(dZ) != 1) placeLeaves(world, x + dX, y + jn, z + dZ);
-
-            if (size == 2 && jn > 0) for (int dX = -2; dX <= 2; dX++)
-                for (int dZ = -2; dZ <= 2; dZ++)
-                {
-                    if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1)
-                    {
-                        if (Math.abs(dX) != 1 || Math.abs(dZ) != 1) placeLeaves(world, x + dX, y + jn, z + dZ);
-
-                        if (jn == 7) placeLeaves(world, x + dX, y + 7, z + dZ);
-                    }
-
-                    if ((Math.abs(dX) != 2 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 1) && (Math.abs(dX) != 1 || Math.abs(dZ) != 2) && inRangeInclusive(jn, 2, 6))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-                }
-
-            if (size == 3 && jn >= 1) for (int dX = -2; dX <= 2; dX++)
-                for (int dZ = -2; dZ <= 2; dZ++)
-                {
-                    if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && (Math.abs(dX) != 1 || Math.abs(dZ) != 1))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-
-                    if ((Math.abs(dX) != 2 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 1) && (Math.abs(dX) != 1 || Math.abs(dZ) != 2) && inRangeInclusive(jn, 2, 10))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-                }
-
-            if (size == 4 && jn >= 1) for (int dX = -3; dX <= 3; dX++)
-                for (int dZ = -3; dZ <= 3; dZ++)
-                {
-                    if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && (Math.abs(dX) != 1 || Math.abs(dZ) != 1))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-
-                    if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && jn <= 14 && jn >= 2)
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-
-                    if (Math.abs(dX) <= 2 && Math.abs(dZ) <= 2 && (Math.abs(dX) != 2 || Math.abs(dZ) != 2) && (jn == 112 || jn == 11 || jn == 3))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-
-                    if ((Math.abs(dX) != 3 || Math.abs(dZ) != 3) && (Math.abs(dX) != 3 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 3) && inRangeInclusive(jn, 4, 10))
-                        placeLeaves(world, x + dX, y + jn, z + dZ);
-                }
-
-            if (jn == height)
+            if (dY >= 1)
             {
-                placeLeaves(world, x, y + jn + 1, z);
-
-                if (size == 3 || size == 4) placeLeaves(world, x, y + jn + 2, z);
+                switch (size)
+                {
+                    case 1:
+                        genSmallLeaves(world, i, j + dY, k);
+                        break;
+                    case 2:
+                        genMediumLeaves(world, i, j, k, dY);
+                        break;
+                    case 3:
+                        genLargeLeaves(world, i, j, k, dY);
+                        break;
+                    default:
+                        genExtraLargeLeaves(world, i, j, k, dY);
+                        break;
+                }
             }
-        }
 
+            if (dY == height) placeLeaves(world, i, j + dY + 1, k);
+            if (dY == height && (size == 4 || size == 3)) placeLeaves(world, i, j + dY + 2, k);
+        }
         return true;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void genExtraLargeLeaves(World world, int x, int y, int z, int dY)
+    {
+        for (int dX = -3; dX <= 3; dX++)
+            for (int dZ = -3; dZ <= 3; dZ++)
+            {
+                if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && (Math.abs(dX) != 1 || Math.abs(dZ) != 1))
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+
+                if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && dY <= 14 && dY >= 2)
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+
+                if (Math.abs(dX) <= 2 && Math.abs(dZ) <= 2 && (Math.abs(dX) != 2 || Math.abs(dZ) != 2) && dY == 12 || dY == 11 || dY == 3)
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+
+                if ((Math.abs(dX) != 3 || Math.abs(dZ) != 3) && (Math.abs(dX) != 3 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 3) && dY <= 10 && dY >= 4)
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+            }
+    }
+
+    private void genLargeLeaves(World world, int x, int y, int z, int dY)
+    {
+        for (int dX = -2; dX <= 2; dX++)
+            for (int dZ = -2; dZ <= 2; dZ++)
+            {
+                if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && (Math.abs(dX) != 1 || Math.abs(dZ) != 1))
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+
+                if ((Math.abs(dX) != 2 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 1) && (Math.abs(dX) != 1 || Math.abs(dZ) != 2) && dY <= 10 && dY >= 2)
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+            }
+    }
+
+    private void genMediumLeaves(World world, int x, int y, int z, int dY)
+    {
+        for (int dX = -2; dX <= 2; dX++)
+            for (int dZ = -2; dZ <= 2; dZ++)
+            {
+                if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && (Math.abs(dX) != 1 || Math.abs(dZ) != 1))
+                {
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+                }
+                if (Math.abs(dX) <= 1 && Math.abs(dZ) <= 1 && dY == 7)
+                {
+                    placeLeaves(world, x + dX, y + 7, z + dZ);
+                }
+                if ((Math.abs(dX) != 2 || Math.abs(dZ) != 2) && (Math.abs(dX) != 2 || Math.abs(dZ) != 1) && (Math.abs(dX) != 1 || Math.abs(dZ) != 2) && dY <= 6 && dY >= 2)
+                {
+                    placeLeaves(world, x + dX, y + dY, z + dZ);
+                }
+            }
+    }
+
+    @SuppressWarnings("MethodWithMultipleLoops")
+    private void genSmallLeaves(World world, int x, int y, int z)
+    {
+        for (int dX = -1; dX <= 1; dX++)
+            for (int dZ = -1; dZ <= 1; dZ++)
+                if (Math.abs(dX) != 1 || Math.abs(dZ) != 1) placeLeaves(world, x + dX, y, z + dZ);
     }
 }
