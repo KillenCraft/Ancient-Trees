3b59c4ec187a502109b0a72bbf29acd77764ce45
Scott Killen <scott@scottkillen.com>
2014-10-31 03:12:59 -0400

    Added porffor blocks

--- src/main/java/com/scottkillen/mod/dendrology/block/ModBlocks.java
+++ src/main/java/com/scottkillen/mod/dendrology/block/ModBlocks.java
@@ -34,12 +34,13 @@ public final class ModBlocks
     private static final ImmutableList<String> LOG0_NAMES = ImmutableList.of("lata", "cedrum", "cerasu", "kiparis");
     private static final ImmutableList<String> LOG1_NAMES = ImmutableList.of("ewcaly", "kulist", "hekur", "maple");
     private static final ImmutableList<String> LOG2_NAMES = ImmutableList.of("palm", "poplar", "walnut", "willow");
+    private static final ImmutableList<String> LOG3_NAMES = ImmutableList.of("porffor");
 
     private static final ImmutableList<String> LEAVES0_NAMES = ImmutableList.of("lata", "cedrum", "kiparis", "ewcaly");
     private static final ImmutableList<String> LEAVES1_NAMES = ImmutableList.of("hekur", "kulist", "palm", "poplar");
     private static final ImmutableList<String> LEAVES2_NAMES =
             ImmutableList.of("cerasu.pink", "cerasu.white", "maple.red", "maple.yellow");
-        private static final ImmutableList<String> LEAVES3_NAMES = ImmutableList.of("kulist.yellow", "walnut", "willow");
+        private static final ImmutableList<String> LEAVES3_NAMES = ImmutableList.of("kulist.yellow", "walnut", "willow", "porffor");
 
     private static final ImmutableList<ModLeavesBlock.Colorizer> LEAVES0_COLORS =
             ImmutableList.of(BASIC, PINE, PINE, BIRCH);
@@ -47,21 +48,22 @@ public final class ModBlocks
             ImmutableList.of(BASIC, BASIC, PINE, BASIC);
     private static final ImmutableList<ModLeavesBlock.Colorizer> LEAVES2_COLORS =
             ImmutableList.of(NONE, NONE, NONE, NONE);
-    private static final ImmutableList<ModLeavesBlock.Colorizer> LEAVES3_COLORS = ImmutableList.of(NONE, BASIC, BIRCH);
+    private static final ImmutableList<ModLeavesBlock.Colorizer> LEAVES3_COLORS = ImmutableList.of(NONE, BASIC, BIRCH, NONE);
 
     private static final ImmutableList<String> SAPLING0_NAMES =
             ImmutableList.of("lata", "cedrum", "cerasu", "kiparis", "ewcaly", "kulist", "hekur", "maple");
-    private static final ImmutableList<String> SAPLING1_NAMES = ImmutableList.of("palm", "poplar", "walnut", "willow");
+    private static final ImmutableList<String> SAPLING1_NAMES = ImmutableList.of("palm", "poplar", "walnut", "willow", "porffor");
 
     private static final ImmutableList<? extends WorldGenerator> SAPLING0_GENS = ImmutableList
             .of(new LataTree(), new CedrumTree(), new CerasuTree(), new KiparisTree(),
                     new EwcalyTree(), new KulistTree(), new HekurTree(), new WorldGenTaiga1());
     private static final ImmutableList<? extends WorldGenerator> SAPLING1_GENS =
-            ImmutableList.of(new WorldGenTaiga1(), new WorldGenTaiga1(), new WorldGenTaiga1(), new WorldGenTaiga1());
+            ImmutableList.of(new WorldGenTaiga1(), new WorldGenTaiga1(), new WorldGenTaiga1(), new WorldGenTaiga1(), new WorldGenTaiga1());
 
     public static Block logs0 = new ModLogBlock(LOG0_NAMES);
     public static Block logs1 = new ModLogBlock(LOG1_NAMES);
     public static Block logs2 = new ModLogBlock(LOG2_NAMES);
+    public static Block logs3 = new ModLogBlock(LOG3_NAMES);
     public static ModLeavesBlock leaves0 = new ModLeavesBlock(LEAVES0_NAMES, LEAVES0_COLORS);
     public static ModLeavesBlock leaves1 = new ModLeavesBlock(LEAVES1_NAMES, LEAVES1_COLORS);
     public static ModLeavesBlock leaves2 = new ModLeavesBlock(LEAVES2_NAMES, LEAVES2_COLORS);
@@ -79,6 +81,7 @@ public final class ModBlocks
         registerLogBlock(logs0, "logs0", LOG0_NAMES);
         registerLogBlock(logs1, "logs1", LOG1_NAMES);
         registerLogBlock(logs2, "logs2", LOG2_NAMES);
+        registerLogBlock(logs3, "logs3", LOG3_NAMES);
         registerLeavesBlock(leaves0, "leaves0");
         registerLeavesBlock(leaves1, "leaves1");
         registerLeavesBlock(leaves2, "leaves2");
@@ -102,8 +105,11 @@ public final class ModBlocks
         // ewcaly
         ModLeavesBlock.addSapling(leaves0, 3, sapling0, 4);
 
-        // ironwood
+        // hekur
         ModLeavesBlock.addSapling(leaves1, 0, sapling0, 6);
+
+        // porffor
+        ModLeavesBlock.addSapling(leaves3, 3, sapling1, 4);
 
         // kulist
         ModLeavesBlock.addSapling(leaves1, 1, sapling0, 5);
