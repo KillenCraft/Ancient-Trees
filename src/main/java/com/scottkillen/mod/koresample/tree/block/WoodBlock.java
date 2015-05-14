e6a0966d292a09c99a485adbd6036b77bc8136de
Scott Killen <scott@scottkillen.com>
2015-05-13 21:28:47 -0400

    Squashed 'api/KoreSample/' changes from a0bfc17..e2efb7d
    e2efb7d Convert tabs to spaces
    4b1cd38 Merge commit 'f1897688c762270b33655415d9342c94f25bf582' into feature/chisel_api_update
    65e06e7 Cleanup code
    eabb5bf Implement Chisel support using new Chisel API.
    414c99a Switched to smaller logo
    72ab305 Added proper credit for the logo.
    dabf1e1 add logo assets
    
    git-subtree-dir: api/KoreSample
    git-subtree-split: e2efb7d68743515e03eb16591187a2817ac080cd

--- src/main/java/com/scottkillen/mod/koresample/tree/block/WoodBlock.java
+++ src/main/java/com/scottkillen/mod/koresample/tree/block/WoodBlock.java
@@ -19,75 +19,76 @@ import java.util.List;
 
 public abstract class WoodBlock extends BlockWood
 {
-    public static final int CAPACITY = 16;
-    private final ImmutableList<DefinesWood> subBlocks;
-    private final List<IIcon> icons = Lists.newArrayList();
+	public static final int CAPACITY = 16;
+	private final ImmutableList<DefinesWood> subBlocks;
+	private final List<IIcon> icons = Lists.newArrayList();
 
-    protected WoodBlock(Collection<? extends DefinesWood> subBlocks)
-    {
-        Preconditions.checkArgument(!subBlocks.isEmpty());
-        Preconditions.checkArgument(subBlocks.size() <= CAPACITY);
-        this.subBlocks = ImmutableList.copyOf(subBlocks);
-        setBlockName("wood");
-    }
+	protected WoodBlock(Collection<? extends DefinesWood> subBlocks)
+	{
+		Preconditions.checkArgument(!subBlocks.isEmpty());
+		Preconditions.checkArgument(subBlocks.size() <= CAPACITY);
+		this.subBlocks = ImmutableList.copyOf(subBlocks);
+		setBlockName("wood");
+	}
 
-    @SuppressWarnings("WeakerAccess")
-    protected static String getUnwrappedUnlocalizedName(String unlocalizedName)
-    {
-        return unlocalizedName.substring(unlocalizedName.indexOf('.') + 1);
-    }
+	@SuppressWarnings("WeakerAccess")
+	protected static String getUnwrappedUnlocalizedName(String unlocalizedName)
+	{
+		return unlocalizedName.substring(unlocalizedName.indexOf('.') + 1);
+	}
 
-    protected final List<DefinesWood> subBlocks() { return Collections.unmodifiableList(subBlocks); }
+	protected final List<DefinesWood> subBlocks() { return Collections.unmodifiableList(subBlocks); }
 
-    public final ImmutableList<String> getSubBlockNames()
-    {
-        final List<String> names = Lists.newArrayList();
-        for (final DefinesWood subBlock : subBlocks)
-            names.add(subBlock.speciesName());
-        return ImmutableList.copyOf(names);
-    }
+	public final ImmutableList<String> getSubBlockNames()
+	{
+		final List<String> names = Lists.newArrayList();
+		for (final DefinesWood subBlock : subBlocks)
+			names.add(subBlock.speciesName());
+		return ImmutableList.copyOf(names);
+	}
 
-    @Override
-    public final String getUnlocalizedName()
-    {
-        return String.format("tile.%s%s", resourcePrefix(), getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
-    }
+	@Override
+	public final String getUnlocalizedName()
+	{
+		return String.format("tile.%s%s", resourcePrefix(), getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public final IIcon getIcon(int unused, int meta)
-    {
-        final int meta1 = meta < 0 || meta >= icons.size() ? 0 : meta;
-        return icons.get(meta1);
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public final IIcon getIcon(int unused, int meta)
+	{
+		final int meta1 = meta < 0 || meta >= icons.size() ? 0 : meta;
+		return icons.get(meta1);
+	}
 
-    @SuppressWarnings("unchecked")
-    @SideOnly(Side.CLIENT)
-    @Override
-    public final void getSubBlocks(Item item, CreativeTabs unused, List subblocks)
-    {
-        for (int i = 0; i < subBlocks.size(); i++)
-            //noinspection ObjectAllocationInLoop
-            subblocks.add(new ItemStack(item, 1, i));
-    }
+	@SuppressWarnings("unchecked")
+	@SideOnly(Side.CLIENT)
+	@Override
+	public final void getSubBlocks(Item item, CreativeTabs unused, List subblocks)
+	{
+		for (int i = 0; i < subBlocks.size(); i++)
+			//noinspection ObjectAllocationInLoop
+			subblocks.add(new ItemStack(item, 1, i));
+	}
 
-    @Override
-    public final void registerBlockIcons(IIconRegister iconRegister)
-    {
-        icons.clear();
+	@SideOnly(Side.CLIENT)
+	@Override
+	public final void registerBlockIcons(IIconRegister iconRegister)
+	{
+		icons.clear();
 
-        for (int i = 0; i < subBlocks.size(); i++)
-        {
-            final String iconName = String.format("%splanks_%s", resourcePrefix(), subBlocks.get(i).speciesName());
-            icons.add(i, iconRegister.registerIcon(iconName));
-        }
-    }
+		for (int i = 0; i < subBlocks.size(); i++)
+		{
+			final String iconName = String.format("%splanks_%s", resourcePrefix(), subBlocks.get(i).speciesName());
+			icons.add(i, iconRegister.registerIcon(iconName));
+		}
+	}
 
-    protected abstract String resourcePrefix();
+	protected abstract String resourcePrefix();
 
-    @Override
-    public String toString()
-    {
-        return Objects.toStringHelper(this).add("subBlocks", subBlocks).add("icons", icons).toString();
-    }
+	@Override
+	public String toString()
+	{
+		return Objects.toStringHelper(this).add("subBlocks", subBlocks).add("icons", icons).toString();
+	}
 }
